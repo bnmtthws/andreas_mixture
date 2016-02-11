139d1d42a9a26b4274cfca54170fc5fcfb2b020e
Ben Matthews <bmatthews@rockefeller.edu>
2016-02-11 00:05:48 -0500

    opening commit

---
+++ KellerVersionSimilarityV7_ben.R
@@ -0,0 +1,70 @@
+require(ggplot2)
+
+# timestamp of the run
+timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
+
+# n of iterations to run
+n = 100000
+
+# Start writing to an output file
+sink(paste (timestamp, "_extreme_mixtures.txt", sep=""))
+
+
+#Calculate the dot product of two vectors
+dot <- function(a,b){
+  x<-sum(a*b)
+  return(x)
+}
+
+#Calculate the norm of a vector
+norm<- function(a){
+  x<-sqrt(sum(a^2))
+  return(x)
+}
+
+#Calculate the angle between two vectors
+angleDist<- function(a,b){
+  x<-acos(dot(a,b)/(norm(a)*norm(b)))
+  return(x)
+}
+
+#read the Dragon descriptors from the file
+odorDesc.21<-read.csv("/scratch/andreas/odorDescriptorsFewer.csv")
+odorDesc.22<-odorDesc.21[,c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22)]
+
+
+.Random.seed
+# Adjust the number of pairs of combinations below
+  mySample <- matrix(nrow=n,ncol=20)
+  mixtureDistances <- vector(length=n)
+  for (i in 1:n){
+#make two mixtures of nonoverlapping components
+  mySample[i,] <- sample(1:nrow(odorDesc.22),20,replace=FALSE)
+  tempMix1<-colSums(odorDesc.22[mySample[i,1:10],])
+  tempMix2<-colSums(odorDesc.22[mySample[i,11:20],])
+#store angledistance
+  mixtureDistances[i] <- angleDist(tempMix1,tempMix2)
+                    
+#only print results for extreme mixtureDistances
+  if(angleDist(tempMix1,tempMix2) < 0.025)
+{
+  print(angleDist(tempMix1,tempMix2))
+  print(mySample[i,1:10])
+  print(tempMix1)
+  print(mySample[i,11:20])
+  print (tempMix2)
+}
+
+if(angleDist(tempMix1,tempMix2) > 0.74)
+{
+  print(angleDist(tempMix1,tempMix2))
+  print(mySample[i,])
+  print(tempMix1)
+  print (tempMix2)
+}
+}
+
+#make the histogram
+mD.df <- data.frame(mixtureDistances)
+hist_finish <-  ggplot(data=mD.df,aes(mixtureDistances))+geom_histogram(binwidth=0.01)+xlab("Angle distance (radians)")+ggtitle(paste(n, "random pairs of 10-component non-overlapping mixtures",sep=" "))
+ggsave(paste (timestamp, ".pdf", sep=""),hist_finish)
